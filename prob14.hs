5d2c2a9a013ba3355f7cf3d79a076eea913d0c28
Ishaaq Chandy <ishaaq@gmail.com>
2010-03-06 06:00:02 +1100

    euler prob14

---
+++ prob14.hs
@@ -0,0 +1,38 @@
+import System(getArgs)
+import System.CPUTime
+import Text.Printf
+import qualified Data.Map as M
+import Data.List
+
+time f = do
+    t0 <- getCPUTime
+    print f
+    t1 <- getCPUTime
+    printf "%.6fs\n" (fromIntegral(t1-t0)/1000000000000 :: Double)
+
+main = do
+    args <- getArgs
+    time $ fst $ maxCollatzSeq 999999
+
+-- most of the complexity below is because of memoization, due to which this program needs
+-- additional heap space to run
+
+
+maxCollatzSeq n = foldl1' max' $ collatzSeqLens n
+       where max' (a, aSeqLen) (b, bSeqLen)
+                | bSeqLen > aSeqLen = (b, bSeqLen)
+                | otherwise = (a, aSeqLen)
+
+
+collatzSeqLens n = M.toList $ M.filterWithKey (\k _ -> k <= n) (foldl' collatzM (M.singleton 1 0) [1..n])
+
+collatzM mp n
+    | M.member n mp = mp
+collatzM mp n = M.insert n (1 + (collatzMNext M.! next)) collatzMNext
+    where next = collatzn n
+          collatzMNext = (collatzM mp next)
+
+collatzn n
+    | even n = n `div` 2
+    | otherwise = 3 * n + 1
+
