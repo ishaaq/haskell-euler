9d655a32ca0ea67a29305b4815fde9c2c24e06af
Ishaaq Chandy <ishaaq@gmail.com>
2010-03-12 17:47:41 +1100

    euler prob27

---
+++ prob27.hs
@@ -0,0 +1,12 @@
+import Timer
+import Maths
+
+main = do
+    time $ snd (foldl (\old@(l', ab') new@(l, ab) -> if (l > l') then new else old) (0, 0) coeffsPrimeLengths)
+
+isPrime n = n `elem` (takeWhile (<= n) primes)
+
+numPrimes a b = length (takeWhile (== True)  [isPrime (n^2 + a * n + b) | n <- [0..]])
+
+coeffsPrimeLengths = [ (l, a * b) | a <- range, b <- range, let l = numPrimes a b] 
+    where range = [-999..999]
